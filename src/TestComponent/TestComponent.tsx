213a4b7e58a1b51e8514a8eeff1c5129a36483a9
Alex Pochebut <alexpochebut96@gmail.com>
2023-10-18 21:23:45 +0300

    init

--- src/TestComponent/TestComponent.tsx
+++ src/TestComponent/TestComponent.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, {ReactNode} from "react";
 
-import { TestComponentProps } from "./TestComponent.types";
 
 import "./TestComponent.css";
 
-const TestComponent: React.FC<TestComponentProps> = ({ heading, content }) => (
-  <div data-testid="test-component" className="test-component">
-    <h1 data-testid="test-component__heading" className="heading">
-      {heading}
-    </h1>
-    <div data-testid="test-component__content">{content}</div>
-  </div>
-);
+export interface TestComponentProps {
+    /**
+     * heading String - heading title to add
+     */
+    heading: string;
+    /**
+     * heading ReactNode - content
+     */
+    content: ReactNode;
+}
+
+const TestComponent: React.FC<TestComponentProps> = ({heading, content}) => {
+    console.log('Error')
+    return <div data-testid="test-component" className="test-component">
+        <h1 data-testid="test-component__heading" className="heading">
+            {heading}
+        </h1>
+        <div data-testid="test-component__content">{content}</div>
+    </div>
+};
 
 export default TestComponent;
